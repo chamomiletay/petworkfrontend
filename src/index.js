1bc0d433b9301b18fc25e563a3e045d702a1b181
chamomiletay <chamomile.tay@gmail.com>
2022-05-22 13:28:13 -0400

    Created router wrap

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//--- import Router component ---//
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+
+//--- Router wrap ---//
+<Router>
     <App />
-  </React.StrictMode>
+</Router>
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
