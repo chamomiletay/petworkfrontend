fb3bb694e49133484eddb721f367d3c78be4774c
senabon <sen.est.diaz@gmail.com>
2022-05-25 19:16:25 -0600

    partial signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,16 +3,29 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div>
-      <h2>SignUp</h2>
+      <h2>Sign Up</h2>
     <form class="username-input">
     <input placeholder="Username" type="text" name="username" required/>
-    </form>
+    </form><br></br>
     <form class="password-input">
     <input placeholder="Password" type="text" name="password" required/>
-    </form>
+    </form><br></br>
     <form class="dogName-input">
     <input placeholder="Dog's Name" type="text" name="dogName" required/>
-    </form>
+    </form><br></br>
+    <form class="dogBreed-input">
+    <input placeholder="Dog's Breed" type="text" name="dogBreed" required/>
+    </form><br></br>
+    <form class="dogBirthday-input">
+    <input placeholder="Dog's Birthday" type="text" name="dogBirthday" required/>
+    </form><br></br>
+    <form class="dogToy-input">
+    <input placeholder="Favorite Toy" type="text" name="dogToy" required/>
+    </form><br></br>
+    <form class="dogDescription-input">
+    <input placeholder="Describe Your Dog!" type="text" name="dogDescription" required/>
+    </form><br></br>
+
 
     
 
