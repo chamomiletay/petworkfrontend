0d17c22602cf256b6c695e2ee402fe265fbd1ad5
senabon <sen.est.diaz@gmail.com>
2022-05-28 13:54:14 -0600

    working on sign in auth

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 
 const SignIn = () => {
 
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] =useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect(()=>{
+    userRef.current.focus();
+  }, [])
+
+  useEffect(()=>{
+    setErrorMessage("");
+  }, [user, password])
+
+  async function handleSubmit(e){
+    e.preventDefault();
+  }
+
   return (
     <div>
+      <p ref={errRef} className={errorMessage ? "errorMessage" : "offscreen"} aria-live="assertive">{errorMessage}</p>
       <h2>Sign In</h2>
-    <form class="username-input" >
-    <input placeholder="Username" type="text" name="username" required />
+    <form className="username-input" onSubmit={handleSubmit} >
+    <input 
+      placeholder="Username" 
+      type="text" 
+      name="username"
+      ref={userRef}
+      autoComplete="off" 
+      onChange = {(e) => setUser(e.target.value)}
+      value={user}
+      required />
     </form>
-    <form class="password-input"><br></br>
-    <input placeholder="Password" type="text" name="password" required />
+
+    <form className="password-input"><br></br>
+    <input 
+      placeholder="Password" 
+      type="password" 
+      name="password" 
+      ref={userRef}
+      onChange = {(e) => setPassword(e.target.value)}
+      value={password}
+      required />
     </form><br></br>
-    <button type="submit"><Link to='/Profile'>Submit</Link></button>
+    <button type="submit">Submit</button>
 
     <p> Don't have an account? 
       <Link to='/SignUp'>
