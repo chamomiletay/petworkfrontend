2b39202e60e7e66a67ff2a883901c5fff3310117
senabon <sen.est.diaz@gmail.com>
2022-05-29 12:18:24 -0600

    auth done

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,16 @@
-import React, {useRef, useState, useEffect} from 'react'
+import React, {useRef, useState, useEffect, useContext} from 'react'
 import { Link } from 'react-router-dom'
+import AuthCon from './SignInAuth';
+import axios from './SignUpAxios';
+
+
+const signinURL = '/profile' //endpoint for signin page
 
 
 const SignIn = () => {
+
+
+  const {setAuth} =useContext(AuthCon)
 
   const userRef = useRef();
   const errRef = useRef();
@@ -21,7 +29,26 @@ const SignIn = () => {
 
   async function handleSubmit(e){
     e.preventDefault();
-  }
+    try{ const response = await axios.post(signinURL, JSON.stringify({username:user, password}),
+      {
+        headers: {'Content-Type': 'application/json' },
+        withCredentials: true 
+      }
+    );
+      console.log(JSON.stringify(response?.data))
+      setAuth({user, password})
+    setUser('');
+    setPassword('');
+  }catch(error){
+    if(!error.response){
+      setErrorMessage('No Response from Server')
+    }else if (error.response?.status ===400){
+      setErrorMessage('Either the username or password is missing')
+    }else{
+      setErrorMessage('Login Failed')
+    }
+    errRef.current.focus();
+  }}
 
   return (
     <div>
