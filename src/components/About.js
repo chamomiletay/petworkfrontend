3a511b43840641373d8fc637d2f8175683d3bb21
senabon <sen.est.diaz@gmail.com>
2022-05-29 23:08:32 -0600

    react smooth scroll partially done

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { AboutContainer, AboutContent, AboutH2, AboutP} from './AboutElements'
+
+const About = () =>{
+    return(
+        <div>
+            <AboutContainer>
+                <AboutContent>
+                    <AboutH2>
+                        About
+                    </AboutH2>
+                    <AboutP>
+                        Petwork is a pet social media inspired by......
+                    </AboutP>
+                </AboutContent>
+            </AboutContainer>
+
+
+        </div>
+
+)
+}
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import {HeaderItems} from './HeaderItems'
+import Video from './videos/Video.mp4'
+import { HomeContainer, HomeBg, VideoBg, HomeContent, HomeH1} from './HomeElements'
+import about from './About'
+import About from './About'
+
 
 const Home = () => {
   return (
     <div>
-        <h1 className='title'>Petwork</h1>
+      <HomeContainer>
+        <HomeBg>
+       
+
+          <VideoBg autoPlay loop muted src={Video} type='viedo/mp4'/>
+
+
         <p>Already a user? <Link to={HeaderItems[2].path}>Sign In</Link></p>
+        </HomeBg>
+        <HomeContent>
+          <HomeH1>Petwork</HomeH1>
+        </HomeContent>
+      </HomeContainer>
+      <About></About>
     </div>
+    
   )
 }
 
