2ba945ccdc54c726a8244f36478ab3c282c2b101
senabon <sen.est.diaz@gmail.com>
2022-06-03 22:05:59 -0600

    navbar and css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,42 +3,53 @@ import * as FaIcons from 'react-icons/fa'
 import { Link } from "react-router-dom";
 import * as AiIcons from 'react-icons/ai'
 import { HeaderItems } from "./HeaderItems";
-import './Sidebar.js'
+import './Sidebar.css'
+import { IconContext } from "react-icons";
+
 
 
 const Sidebar = ()=>{
     const [navbar, setNavbar] = useState(false)
 
-    const showNavbar =()=> setNavbar(!navbar);
+    //const showNavbar = () => setNavbar(!navbar);
 
     return(
-        <>
-        <div className='navbar'>
-            <Link to='' className="menu-bars">
-                <FaIcons.FaBars onClick={showNavbar}/>
+        <div className='nabar'>
+       
+            
+        <Link to='#'className="menu-bars">
+            <IconContext.Provider value={{className:'react-icons bars'}}>
+                <FaIcons.FaBars />
+            </IconContext.Provider>
+    
+            
             </Link>
-
-        </div>
-        <nav className={navbar ? 'nav-menu active': 'nav-manu'}>
-            <ul className="nav-menu-items">
+        <div className="navbar">
+        <nav className={navbar ? 'nav-menu active': 'nav-menu'}>
+            <ul className="nav-menu-items" >
                 <li className="navbar-toggle">
-                  <Link to='' className='menu-bars'>
-                        <AiIcons.AiOutlineClose/>
-                    </Link>  
+                  <Link to='#' className='menu-bars'>
+                      <IconContext.Provider value={{className:'react-icons ex'}}>
+                        <AiIcons.AiOutlineClose />
+                    </IconContext.Provider>
+                    </Link> 
                 </li>
                 {HeaderItems.map((item,index) =>{
                     return(
                         <li key={index} className={item.cName}>
+                            <IconContext.Provider value={{className:'react-icons'}}>
                             <Link to={item.path}>
                             {item.icon}
-                            <span>{item.title}</span>
+                            <span className="title">{item.title}</span>
                             </Link>
+                            </IconContext.Provider>
                         </li>
                     )
                 })}
             </ul>
         </nav>
-        </>
+        </div>
+        </div>
     )
 }
 
