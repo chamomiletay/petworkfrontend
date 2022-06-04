32af4e628d2db03b72202712a5e3e370fcd1c239
bbkc27 <briana.j.casey@gmail.com>
2022-06-04 11:25:47 -0400

    Fixed header issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,9 +35,9 @@ const Header = () => {
               )
               })}
 
-            {/* <LinkRouter to={ userInfo ? `/profile/${userInfo.username}` : '/signin'} className="pages profile">{userInfo? <li>Profile</li> : <li>Sign In</li>}</LinkRouter>
-            <span className="pages bar">|</span> */}
-            <LinkRouter to={`/profile/${userInfo.username}`} className="pages profile">Profile</LinkRouter>
+            <LinkRouter to={ userInfo ? `/profile/${userInfo.username}` : '/signin'} className="pages profile">{userInfo? <li>Profile</li> : <li>Sign In</li>}</LinkRouter>
+            <span className="pages bar">|</span>
+            {/* <LinkRouter to={`/profile/${userInfo.username}`} className="pages profile">Profile</LinkRouter> */}
 
             <LinkRouter to="/favorites" className="pages profile"><li>Favorites</li></LinkRouter>
 
